4c0775cf746611aeb23fc1c0c892e8c9dfe8cf01
Juan David Arias <juadariasmar@gmail.com>
2026-01-12 20:55:51 -0500

    practice

--- funcion-2.py
+++ funcion-2.py
@@ -1,10 +1,10 @@
-import math
-
 def pendiente(x1, y1, x2, y2):
     return (y2-y1)/(x2-x1)
 
 def intercepta(x1, y1, x2, y2):
-    return 
+    return y1 - pendiente(x1, y1, x2, y2) * x1
 
+
+print(intercepta(1,3, 2, 6))
 
 
