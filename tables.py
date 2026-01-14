ba956de874e2d300a216294dc1c149073e591402
Juan David Arias <juadariasmar@gmail.com>
2026-01-14 04:20:38 -0500

    Update tables.py

--- tables.py
+++ tables.py
@@ -1,11 +1,12 @@
 import math
 
 def tables():
+
     x = 1.0
 
     while x < 10.0:
-        print(x), '\t', math.log(x) 
-        x = x + 1.0
+        print(f"{x:>2.0f}\t{math.log(x):>20.11f}") 
+        x += 1
 
 tables()
 
