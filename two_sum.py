fbac8b321f41710f38d886c5e8425395dbd56efd
Juan David Arias <juadariasmar@gmail.com>
2026-01-13 23:09:49 -0500

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -2,7 +2,7 @@ def two_sum_brute_force(nums, target):
 
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
-            if nums[j] == target - nums[i]:
+            if nums[i] + nums[j] == target:
                 return [i, j]
             
     return []
