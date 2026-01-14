c5f1387682320719c46b5b471cef19308a844760
Juan David Arias <juadariasmar@gmail.com>
2026-01-14 14:47:44 -0500

    loop practice

--- tables.py
+++ tables.py
@@ -4,8 +4,15 @@ def tables():
 
     x = 1.0
 
-    while x < 10.0:
-        print(f"{x:>2.0f}\t{math.log(x):>20.11f}") 
+    while x < 2**16:
+
+        try:
+            valor = math.exp2(x)    
+            print(f"{x:>6.0f}\t{valor:>20.11f}") 
+
+        except OverflowError:
+            print(f"{x:>6.0f}\t{'inf':>20}")
+            
         x += 1
 
 tables()
